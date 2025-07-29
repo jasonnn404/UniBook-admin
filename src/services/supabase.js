14d6307b375247aa2b75d64f37a0bc3b95f96fad
Jason Li <jasonnn404l@gmail.com>
2025-07-29 17:41:53 -0400

    Remove hardcoded Supabase key and use .env

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://minmdhmltjguwoioatmi.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pbm1kaG1sdGpndXdvaW9hdG1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NjAzMzAsImV4cCI6MjA2NDEzNjMzMH0.oMSed2Y1bl8GIT53kzx5s-u8u6S6dnNQzhfGAtBo0AA";
+
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseKey = import.meta.env.SUPABASE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export { supabaseUrl };
